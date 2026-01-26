620c89f84c517990635ac00092f9f6ac38615913
Walle692 <146271763+Walle692@users.noreply.github.com>
2026-01-26 16:01:15 +0100

    login start

---
+++ BackEnd/version1/services/authenticateuser.go
@@ -0,0 +1,51 @@
+package services
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"version1/utils"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func AuthenticateUser(username, password string) (string, error) {
+	// set up connection to database, the DATABASE url should be stored in env
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+
+	// if error
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to connect to database %v\n", err)
+		os.Exit(1)
+	}
+
+	// makes sure that connection is closed after authenticate user is done running (closes on return statement)
+	defer conn.Close(context.Background())
+
+	// variable to store the Stored password if the user exists
+	var storedPassword string
+
+	// query the database (mby use query instead of queryrow?) and select the password from the username and store in storedpassword
+	err = conn.QueryRow(context.Background(), "SELECT pword FROM users WHERE uname= $1", username).Scan(&storedPassword)
+
+	if err == pgx.ErrNoRows {
+		// no user found
+		return "", errors.New("No user found")
+	} else if err != nil {
+		// other error
+	}
+
+	if password != storedPassword {
+		// passwords didn't match
+		return "", errors.New("invalid password")
+	}
+
+	// Generate JWT token
+	token, err := utils.GenerateJWT(username)
+	if err != nil {
+		return "", errors.New("failed to generate token")
+	}
+
+	return token, nil
+}
