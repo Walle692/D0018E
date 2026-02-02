4be984330a2e0a908654fbd48a0af2346bd50d65
Walle692 <146271763+Walle692@users.noreply.github.com>
2026-02-02 16:16:48 +0100

    fixed some small things to get stuff to work

--- BackEnd/version1/utils/tokentodb.go
+++ BackEnd/version1/utils/tokentodb.go
@@ -9,7 +9,7 @@ import (
 func (pg *Postgres) TokenToDB(ctx context.Context, token string, expires_at time.Time, username string) error {
 
 	// query
-	query := "UPDATE myschema.test SET token=$1, token_expires_at=$2 WHERE uname=$3"
+	query := "UPDATE myschema.users SET token=$1, token_expires_at=$2 WHERE uname=$3"
 
 	// execute query
 	_, err := pg.db.Exec(ctx, query, token, expires_at, username)
