430d6c6e3c9b074ab5092b0b6762ed84b5830822
Walle692 <146271763+Walle692@users.noreply.github.com>
2026-02-09 11:07:32 +0100

    Changed specific product, vibecoded some frontend not tested

--- BackEnd/version2/services/specificproduct.go
+++
@@ -1,9 +0,0 @@
-package services
-
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func GetSpecificProduct(c *gin.Context) {
-
-}
---
+++ BackEnd/version2/utils/specificproduct.go
@@ -0,0 +1,61 @@
+package utils
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"github.com/walle692/D0018E/BackEnd/version2/global"
+)
+
+func GetSpecificProduct(c *gin.Context, productID int) ([]byte, error) {
+	ctx := context.Background()
+
+	type ProductStruct struct {
+		Product_id     int     `json:"product_id"`
+		Product_name   string  `json:"product_name"`
+		Manufacturer   string  `json:"manufacturer"`
+		Seller_user_id int     `json:"seller_user_id"`
+		Description    string  `json:"description"`
+		Screen_size    float32 `json:"screen_size"`
+		Picture_url    string  `json:"picture_url"`
+		Sku            string  `json:"sku"`
+		Price          float32 `json:"price"`
+		Stock          int     `json:"stock"`
+	}
+
+	var p ProductStruct
+
+	// query
+	query := "SELECT * FROM myschema.products WHERE product_id=$1"
+
+	// get the db connection from global
+	postgres := global.Get()
+
+	// query the database and select the password from the username and store in password
+	err := postgres.Pool().QueryRow(ctx, query, productID).Scan(&p.Product_id, &p.Product_name, &p.Manufacturer, &p.Seller_user_id, &p.Description, &p.Screen_size, &p.Picture_url, &p.Sku, &p.Price, &p.Stock)
+
+	if err == pgx.ErrNoRows {
+		fmt.Println("DEBUG: PRODUCT ERR NO ROWS")
+		// no user found
+		return make([]byte, 0), errors.New("No user found")
+	} else if err != nil {
+		fmt.Println("DEBUG: PRODUCT OTHER GET ERROR")
+		fmt.Println(err)
+		// other error
+		return make([]byte, 0), errors.New("Unhandled product error")
+	}
+
+	// make the struct into acutal json
+	productJSON, err := json.Marshal(p)
+	if err != nil {
+		fmt.Println("DEBUG: PRODUCT JSON MARSHAL ERROR")
+		fmt.Println(err)
+		return make([]byte, 0), errors.New("couldn't marshal json")
+	}
+
+	return productJSON, nil
+}
