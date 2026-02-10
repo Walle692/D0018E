4d0220cdac7d448128a2857696d7c803c8515e20
Leo Man <leoman3628@gmail.com>
2026-02-10 19:57:15 +0100

    makeuser in seperate file

---
+++ BackEnd/version2/services/makeuser.go
@@ -0,0 +1,31 @@
+package services
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/walle692/D0018E/BackEnd/version2/utils"
+)
+
+type CreateUserRequest struct {
+	Username string `json:"username" binding:"required,min=3"`
+	Password string `json:"password" binding:"required,min=3"`
+	Role     string `json:"role"     binding:"required,oneof=buyer seller admin"`
+}
+
+func MakeUser(c *gin.Context) {
+	// Read input from request body
+	var req CreateUserRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := utils.CreateUser(req.Username, req.Password, req.Role); err != nil {
+		// optionally detect duplicate username, etc.
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "could not create user"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"status": "created"})
+}
--- BackEnd/version2/services/testservice.go
+++ BackEnd/version2/services/testservice.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/walle692/D0018E/BackEnd/version2/global"
-	"github.com/walle692/D0018E/BackEnd/version2/utils"
 )
 
 // returns the username
@@ -20,27 +19,4 @@ func Me(c *gin.Context) {
 // status will tell the user whether it is logged in or not.
 func Status(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": "You are logged in"})
-}
-
-type CreateUserRequest struct {
-	Username string `json:"username" binding:"required,min=3"`
-	Password string `json:"password" binding:"required,min=3"`
-	Role     string `json:"role"     binding:"required,oneof=buyer seller admin"`
-}
-
-func MakeUser(c *gin.Context) {
-	// Read input from request body
-	var req CreateUserRequest
-	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	if err := utils.CreateUser(req.Username, req.Password, req.Role); err != nil {
-		// optionally detect duplicate username, etc.
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "could not create user"})
-		return
-	}
-
-	c.JSON(http.StatusCreated, gin.H{"status": "created"})
 }
