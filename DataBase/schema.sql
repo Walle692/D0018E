27db577e85cb16c655750362f751a1b5f2e5bc5a
Walle692 <146271763+Walle692@users.noreply.github.com>
2026-02-19 13:31:49 +0100

    update schema.sql

--- DataBase/schema.sql
+++ DataBase/schema.sql
@@ -6,28 +6,29 @@ DROP TABLE IF EXISTS myschema.order;
 DROP TABLE IF EXISTS myschema.orderitem;
 DROP TABLE IF EXISTS myschema.basket;
 DROP TABLE IF EXISTS myschema.basketitem;
+DROP TABLE IF EXISTS myschema.rating;
 
 CREATE TABLE IF NOT EXISTS myschema.users (
     user_id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    role VARCHAR(10) NOT NULL,
-    address VARCHAR(50),
-    country VARCHAR(37),
+    username VARCHAR UNIQUE NOT NULL,
+    password VARCHAR NOT NULL,
+    role VARCHAR NOT NULL,
+    address VARCHAR,
+    country VARCHAR,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS myschema.products (
     product_id SERIAL PRIMARY KEY,
-    product_name VARCHAR(100) NOT NULL,
-    manufacturer VARCHAR(50) NOT NULL,
+    product_name VARCHAR NOT NULL,
+    manufacturer VARCHAR NOT NULL,
     seller_user_id INT NOT NULL,
-    description VARCHAR(400),
-    screen_size FLOAT NOT NULL,
-    picture_url VARCHAR(400),
-    sku VARCHAR(50),
+    description VARCHAR,
+    screen_size FLOAT,
+    picture_url VARCHAR,
     price FLOAT NOT NULL,
     stock INT NOT NULL,
+    active BOOLEAN DEFAULT TRUE,
     FOREIGN KEY (seller_user_id) REFERENCES myschema.users(user_id)
 );
 
@@ -60,22 +61,15 @@ CREATE TABLE IF NOT EXISTS myschema.basketitem (
     basket_id INT NOT NULL,
     product_id INT NOT NULL,
     quantity INT NOT NULL,
-    price FLOAT NOT NULL,
     FOREIGN KEY (basket_id) REFERENCES myschema.basket(basket_id),
     FOREIGN KEY (product_id) REFERENCES myschema.products(product_id)
 );
 
+CREATE TABLE IF NOT EXITS myschema.rating (
+    comment_id SERIAL PRIMARY KEY,
+    customer_user_id INT NOT NULL,
+    product_id INT NOT NULL,
+    comment VARCHAR DEFAULT '',
+    rating INT CHECK (rating >= 1 AND rating <= 3),
+)
 
-/* Legacy incase of nuke
-CREATE TABLE IF NOT EXISTS myschema.users (
-    username VARCHAR(50) UNIQUE NOT NULL PRIMARY KEY,
-    password VARCHAR(50) NOT NULL,
-    role VARCHAR(10) NOT NULL,
-    token VARCHAR(256),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS myschema.token (
-    username VARCHAR(50) REFERENCES myschema.users(username) PRIMARY KEY,
-    token    VARCHAR(255) NOT NULL
-);*/
