6006ae62b699c32d2fd911bbed6f82bee02c632c
Walle692 <146271763+Walle692@users.noreply.github.com>
2026-02-19 13:59:06 +0100

    test fix add to basket

--- BackEnd/version2/utils/createbasketitem.go
+++ BackEnd/version2/utils/createbasketitem.go
@@ -13,18 +13,20 @@ func CreateBasketItem(c *gin.Context, basketID int, productID int, quantity int)
 
 	postgres := global.Get()
 
-	// start by getting the price of the product from the database
-	var price int
-	query := "SELECT price FROM myschema.products WHERE product_id=$1"
-	err := postgres.Pool().QueryRow(c, query, productID).Scan(&price)
-	if err != nil {
-		fmt.Println("DEBUG: CREATE BASKET ITEM GET PRICE ERROR")
-		return err
-	}
+	/*
+		// start by getting the price of the product from the database
+		var price int
+		query := "SELECT price FROM myschema.products WHERE product_id=$1"
+		err := postgres.Pool().QueryRow(c, query, productID).Scan(&price)
+		if err != nil {
+			fmt.Println("DEBUG: CREATE BASKET ITEM GET PRICE ERROR")
+			return err
+		}
+	*/
 
 	// insert the basketitem into the db
-	query = "INSERT INTO myschema.basketitem (basket_id, product_id, quantity, price) VALUES ($1, $2, $3, $4)"
-	_, err = postgres.Pool().Exec(ctx, query, basketID, productID, quantity, price)
+	query := "INSERT INTO myschema.basketitem (basket_id, product_id, quantity) VALUES ($1, $2, $3, $4)"
+	_, err := postgres.Pool().Exec(ctx, query, basketID, productID, quantity)
 	if err != nil {
 		fmt.Println("DEBUG: CREATE BASKET ITEM INSERT ERROR")
 		return err
