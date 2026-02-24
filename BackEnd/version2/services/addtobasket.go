37ffad9638eb6531d0772e34c94494960c7eb5a6
Leo Man <leoman3628@gmail.com>
2026-02-24 04:31:20 +0100

    Refactor to pass userID over basketID

--- BackEnd/version2/services/addtobasket.go
+++ BackEnd/version2/services/addtobasket.go
@@ -3,8 +3,11 @@ package services
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"github.com/walle692/D0018E/BackEnd/version2/global"
 	"github.com/walle692/D0018E/BackEnd/version2/utils"
 )
 
@@ -17,23 +20,21 @@ type AddToBasketRequest struct {
 func AddToBasket(c *gin.Context) {
 	// read input from request body
 	var req AddToBasketRequest
+	session := sessions.Default(c)
+	userIDStr := session.Get(global.UserID)
+	userID, err := strconv.Atoi(userIDStr.(string))
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
 
 	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	fmt.Printf("DEBUG: AddToBasketRequest: %+v\n", req)
-
-	// get the users basket id
-	basketID, err := utils.GetBasketID(c)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "could not get basket"})
-		return
-	}
-
 	// create the basketItem in the database
-	if err := utils.CreateBasketItem(c, basketID, req.ProductID, req.Quantity); err != nil {
+	if err := utils.CreateBasketItem(userID, req.ProductID, req.Quantity); err != nil {
 		fmt.Println("DEBUG: CREATE BASKET ITEM ERROR")
 		fmt.Println(err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "could not add item to basket"})
--- BackEnd/version2/utils/createbasketitem.go
+++ BackEnd/version2/utils/createbasketitem.go
@@ -4,29 +4,22 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/gin-gonic/gin"
 	"github.com/walle692/D0018E/BackEnd/version2/global"
 )
 
-func CreateBasketItem(c *gin.Context, basketID int, productID int, quantity int) error {
+func CreateBasketItem(userID int, productID int, quantity int) error {
 	ctx := context.Background()
 
-	postgres := global.Get()
-
-	/*
-		// start by getting the price of the product from the database
-		var price int
-		query := "SELECT price FROM myschema.products WHERE product_id=$1"
-		err := postgres.Pool().QueryRow(c, query, productID).Scan(&price)
-		if err != nil {
-			fmt.Println("DEBUG: CREATE BASKET ITEM GET PRICE ERROR")
-			return err
-		}
-	*/
+	pool := global.Get().Pool()
 
 	// insert the basketitem into the db
-	query := "INSERT INTO myschema.basketitem (basket_id, product_id, quantity) VALUES ($1, $2, $3)"
-	_, err := postgres.Pool().Exec(ctx, query, basketID, productID, quantity)
+	query := `
+	INSERT INTO myschema.basketitem (basket_id, product_id, quantity)
+	SELECT b.basket_id, $2, $3
+	FROM myschema.basket b
+	WHERE b.basket_user_id = $1
+	`
+	_, err := pool.Exec(ctx, query, userID, productID, quantity)
 	if err != nil {
 		fmt.Println("DEBUG: CREATE BASKET ITEM INSERT ERROR")
 		return err
