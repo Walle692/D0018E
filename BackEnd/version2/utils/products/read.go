debc43dc80da1617a5b5a55c0b4c4970084bdd04
Leo Man <leoman3628@gmail.com>
2026-02-25 10:23:01 +0100

    Product multiline read using ListByQuery function

--- BackEnd/version2/utils/products/read.go
+++ BackEnd/version2/utils/products/read.go
@@ -3,36 +3,38 @@ package products
 import (
 	"context"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/walle692/D0018E/BackEnd/version2/global"
+	queries "github.com/walle692/D0018E/BackEnd/version2/utils"
 )
 
 func ListAll() ([]Product, error) {
-	ctx := context.Background()
-	pool := global.Get().Pool()
 
-	query := `SELECT * FROM myschema.products WHERE active=true`
+	query := `
+		SELECT product_id, product_name, manufacturer, description, screen_size, picture_url, price, stock  
+		FROM myschema.products
+		WHERE active = true
+	`
+	scanProduct := func(rows pgx.Rows) (Product, error) {
+		var p Product
+		err := rows.Scan(
+			&p.Product_id,
+			&p.Product_name,
+			&p.Manufacturer,
+			&p.Description,
+			&p.Screen_size,
+			&p.Picture_url,
+			&p.Price,
+			&p.Stock,
+		)
+		return p, err
+	}
 
-	products := make([]Product, 0)
-
-	rows, err := pool.Query(ctx, query)
-
+	p, err := queries.ListByQuery(query, scanProduct)
 	if err != nil {
 		return nil, err
 	}
-	defer rows.Close()
-	for rows.Next() {
-		var p Product
-		if err := rows.Scan(&p.Product_id, &p.Product_name, &p.Manufacturer, &p.Seller_user_id, &p.Description,
-			&p.Screen_size, &p.Picture_url, &p.Price, &p.Stock, &p.Active); err != nil {
-			return nil, err
-		}
-		products = append(products, p)
-	}
-	if err = rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return products, nil
+	return p, nil
 }
 
 func GetByProductID(productID int) (Product, error) {
@@ -52,32 +54,29 @@ func GetByProductID(productID int) (Product, error) {
 }
 
 func ListBySellerID(sellerID int) ([]Product, error) {
-	ctx := context.Background()
-	pool := global.Get().Pool()
-
-	products := make([]Product, 0)
-
-	rows, err := pool.Query(ctx, `
-		SELECT product_id, product_name, picture_url, price, stock, active, screen_size, description, manufacturer
+	query := `
+		SELECT product_name, manufacturer, description, screen_size, picture_url, price, stock  
 		FROM myschema.products
 		WHERE seller_user_id = $1 AND active = true
-	`, sellerID)
+	`
 
+	scanProduct := func(rows pgx.Rows) (Product, error) {
+		var p Product
+		err := rows.Scan(
+			&p.Product_name,
+			&p.Manufacturer,
+			&p.Description,
+			&p.Screen_size,
+			&p.Picture_url,
+			&p.Price,
+			&p.Stock,
+		)
+		return p, err
+	}
+
+	p, err := queries.ListByQuery(query, scanProduct, sellerID)
 	if err != nil {
 		return nil, err
 	}
-	defer rows.Close()
-	for rows.Next() {
-		var s Product
-		if err := rows.Scan(&s.Product_id, &s.Product_name, &s.Picture_url, &s.Price, &s.Stock,
-			&s.Active, &s.Screen_size, &s.Description, &s.Manufacturer); err != nil {
-			return nil, err
-		}
-		products = append(products, s)
-	}
-	if err = rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return products, nil
+	return p, nil
 }
